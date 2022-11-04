1e4a6cb164eefb7507a22c9e38892bd32feba88b
stankoto <stankotovych@gmail.com>
2022-11-05 00:34:53 +0200

    Create html pages and client logic

---
+++ public/js/getFirstUser.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const user = document.querySelector('.display-user');
+const navLinks = document.querySelector('.nav-links');
+const error = document.querySelector('.error');
+
+fetch(document.location.origin + '/api/v1/users/1')
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      user.innerHTML = `
+        <h1>User #${data.user.id}</h1>
+        <span>
+          <ul>
+            <li>id: ${data.user.id}</li>
+            <li>name: ${data.user.name}</li>
+            <li>email: ${data.user.email}</li>
+            <li>phone: ${data.user.phone}</li>
+            <li>position: ${data.user.position}</li>
+            <li>position_id: ${data.user.position_id}</li>
+          </ul>
+          <img src="${data.user.photo}" alt="User photo" crossorigin="anonymous">
+        </span>
+      `;
+      navLinks.hidden = false;
+    } else {
+      let failInfo = `<h1>${data.message}</h1>`;
+      if (data.fails) {
+        failInfo += '<p>Fails:</p><ul>'
+        for (const key of Object.keys(data.fails)) {
+          failInfo += `<li>${key}: ${data.fails[key]}</li>`;
+        }
+        failInfo += '</ul>';
+      }
+      error.innerHTML = failInfo;
+    }
+  })
+  .catch(err => console.error(err));
---
+++ public/js/getUsers.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const users = document.querySelector('.display-users');
+const error = document.querySelector('.error');
+
+(function getUsers (url) {
+  users.innerHTML = '';
+  document.querySelector('.previous-page').hidden = true;
+  document.querySelector('.previous-page').replaceWith(document.querySelector('.previous-page').cloneNode(true));
+  const previousPage = document.querySelector('.previous-page');
+  document.querySelector('.next-page').hidden = true;
+  document.querySelector('.next-page').replaceWith(document.querySelector('.next-page').cloneNode(true));
+  const nextPage = document.querySelector('.next-page');
+
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      const count = data.count < data.total_users ? data.count : data.total_users;
+      let usersHTML = `
+        <h1>Page ${data.page} of ${data.total_pages}, showing ${count} of ${data.total_users} users</h1>
+        <ul>
+      `;
+      for (const user of data.users) {
+        usersHTML += `
+          <li>
+            <h1>User #${user.id}</h1>
+            <span>
+              <ul>
+                <li>id: ${user.id}</li>
+                <li>name: ${user.name}</li>
+                <li>email: ${user.email}</li>
+                <li>phone: ${user.phone}</li>
+                <li>position: ${user.position}</li>
+                <li>position_id: ${user.position_id}</li>
+                <li>created_at: ${user.createdAt}</li>
+                <li>updated_at: ${user.updatedAt}</li>
+              </ul>
+              <img src="${user.photo}" alt="User photo" crossorigin="anonymous">
+            </span>
+          </li>
+        `;
+      }
+      users.innerHTML = usersHTML + '</ul>';
+      if (data.links.prev_url !== null) {
+        previousPage.addEventListener('click', e => {
+          e.preventDefault();
+          getUsers(data.links.prev_url);
+        })
+        previousPage.hidden = false;
+      }
+      if (data.links.next_url !== null) {
+        nextPage.addEventListener('click', e => {
+          e.preventDefault();
+          getUsers(data.links.next_url);
+        })
+        nextPage.hidden = false;
+      }
+    } else {
+      let failInfo = `<h1>${data.message}</h1>`;
+      if (data.fails) {
+        failInfo += '<p>Fails:</p><ul>'
+        for (const key of Object.keys(data.fails)) {
+          failInfo += `<li>${key}: ${data.fails[key]}</li>`;
+        }
+        failInfo += '</ul>';
+      }
+      error.innerHTML = failInfo;
+    }
+  })
+  .catch(err => console.error(err));
+}) (document.location.origin + '/api/v1/users?count=6')
